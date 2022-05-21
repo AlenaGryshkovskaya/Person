9ed84ad7f94f3e8ddad3f7fe16d775815de473d3
galart19 <galart086@mail.ru>
2022-05-21 08:51:10 +0300

    hgv

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let names = [
+/*let names = [
     'Маша',
     'Паша',
     'Женя',
@@ -6,6 +6,71 @@ let names = [
     'Саша',
     'Катя'
 ]
-console.log(names);
-console.log(names.length);
-console.log(names[]);
+let l_names = [
+    'Иванов',
+    'Васильева',
+    'Ващенко',
+    'Михалец',
+    'Данилов',
+    'Фадеев'
+]
+
+let randIndexNames = Math.floor(Math.random() * (names.length - 1));
+let randIndexL_Names = Math.floor(Math.random() * (names.length - 1));
+console.log(l_names[randIndexL_Names], names[randIndexNames]);*/
+
+//console.log(randIndex);
+//console.log(names.length);
+//console.log(names[names.length - 1]);
+//console.log(names[names.length - 1]);
+//console.log(names[randIndex]);
+
+
+let women = [
+    l_name = [
+        'Ерохина',
+        'Иванова',
+        'Шишкина',
+        'Кондакова',
+        'Романова'
+    ],
+    name = [
+        'Маша',
+        'Катя',
+        'Настя',
+        'Варя',
+        'Оля'
+    ],
+    otchestvo = [
+        'Афанасиевна',
+        'Богдановна',
+        'Даниловна',
+        'Нестеровна',
+        'Дмитриевна'
+    ],
+]
+let men = [
+    l_name = [
+        'Боталов',
+        'Михалец',
+        'Трунев',
+        'Шевченко',
+        'Волков'
+    ],
+    name = [
+        'Ваня',
+        'Даня',
+        'Саша',
+        'Миша',
+        'Рома'
+    ],
+    otchestvo = [
+        'Харитонович',
+        'Серафимович',
+        'Михеевич',
+        'Владиславович',
+        'Андреевич',
+    ],
+]
+
+let randomNumber = Math.floor(Math.random())

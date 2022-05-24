e38a8935c7ef64f86b058486f680eecfc5aec212
galart19 <galart086@mail.ru>
2022-05-24 11:06:52 +0300

    Мы вывели из js информацию в html

--- script.js
+++ script.js
@@ -25,81 +25,97 @@ console.log(l_names[randIndexL_Names], names[randIndexNames]);*/
 //console.log(names[names.length - 1]);
 //console.log(names[randIndex]);
 
-let randomAge = Math.floor(Math.random() * 60) + 1;
-let randomWeigth = Math.floor(Math.random() * 100) + 40;
+let generation = document.querySelector('.generation');
 
-let womenW = [
-    l_name = [
+
+let womenW = {
+    l_name: [
         'Ерохина',
         'Иванова',
         'Шишкина',
         'Кондакова',
         'Романова'
     ],
-    name = [
+    name: [
         'Маша',
         'Катя',
         'Настя',
         'Варя',
         'Оля'
     ],
-    otchestvo = [
+    otchestvo: [
         'Афанасиевна',
         'Богдановна',
         'Даниловна',
         'Нестеровна',
         'Дмитриевна'
     ],
-    uniqueAbilies = [
+    uniqueAbilies: [
         'Быстро читает',
         'Быстро бегает',
         'Танцует',
         'Поет',
         'Сочиняет стихи'
     ]
-]
-let men = [
-    l_name = [
+}
+let manW = {
+    l_name: [
         'Боталов',
         'Михалец',
         'Трунев',
         'Шевченко',
         'Волков'
     ],
-    name = [
+    name: [
         'Ваня',
         'Даня',
         'Саша',
         'Миша',
         'Рома'
     ],
-    otchestvo = [
+    otchestvo: [
         'Харитонович',
         'Серафимович',
         'Михеевич',
         'Владиславович',
         'Андреевич',
     ],
-    uniqueAbilies = [
+    uniqueAbilies: [
         'Очень сильный',
         'Читает рэп',
         'Водит машину',
         'Быстро собирается',
         'Играет в кс'
     ]
-]
+}
 
 let gender = [
     'women',
-    'men'
+    'man'
 ]
 
-let genders = gender[Math.floor(Math.random() * (gender.length))];
 
-if (genders === "women") {
-    let randomL_name = Math.floor(Math.random() * (womenW.l_name.length - 1));
-    let randomName = Math.floor(Math.random() * (womenW.name.length - 1));
-    let randomOtchestvo = Math.floor(Math.random() * (womenW.Otchestvo.length - 1));
-    let uniqueAbilies = Math.floor(Math.random() * (womenW.uniqueAbilies.length - 1));
-}
-пщоалдвоавопо
+
+generation.addEventListener('click', function (){
+    let randomAge = Math.floor(Math.random() * 60) + 10;
+    let randomWeigth = Math.floor(Math.random() * 60) + 40;
+    let genders = gender[Math.floor(Math.random() * (gender.length))];
+    if (genders === "women") {
+        let randomL_name = Math.floor(Math.random() * (womenW.l_name.length - 1));
+        let randomName = Math.floor(Math.random() * (womenW.name.length - 1));
+        let randomOtchestvo = Math.floor(Math.random() * (womenW.otchestvo.length - 1));
+        let randomUniqueAbilies = Math.floor(Math.random() * (womenW.uniqueAbilies.length - 1));
+        document.getElementById("info").innerHTML = 'ФИО: ' + womenW.l_name[randomL_name]+ ' ' + womenW.name[randomName] + ' ' +
+            womenW.otchestvo[randomOtchestvo] + '<br>' + 'Пол: ' + 'Женщина' + '<br>' + 'Возраст: ' + randomAge +
+            '<br>' + 'Вес: ' + randomWeigth + '<br>' + 'Уникальная способность: ' + womenW.uniqueAbilies[randomUniqueAbilies];
+    } else if (genders === "man") {
+        let randomL_name = Math.floor(Math.random() * (manW.l_name.length - 1));
+        let randomName = Math.floor(Math.random() * (manW.name.length - 1));
+        let randomOtchestvo = Math.floor(Math.random() * (manW.otchestvo.length - 1));
+        let randomUniqueAbilies = Math.floor(Math.random() * (manW.uniqueAbilies.length - 1));
+        document.getElementById("info").innerHTML = 'ФИО: ' + manW.l_name[randomL_name]+ ' ' + manW.name[randomName] + ' ' +
+            manW.otchestvo[randomOtchestvo] + '<br>' + 'Пол: ' + 'Мужчина' + '<br>' + 'Возраст: ' + randomAge +
+            '<br>' + 'Вес: ' + randomWeigth + '<br>' + 'Уникальная способность: ' + manW.uniqueAbilies[randomUniqueAbilies];
+    }
+})
+//document.getElementById("info").innerHTML=generation;
